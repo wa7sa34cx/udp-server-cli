de79e6d93500370b692b44fa9114e1087f45fc4e
Alex Black <wa7@sa34.cx>
2021-12-05 12:54:39 +0300

    Add DB migrations

---
+++ migrations/20211205072241_items_table.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS items
+(
+    id      INTEGER PRIMARY KEY NOT NULL,
+    text    TEXT                NOT NULL
+);
---
+++ migrations/20211205072431_fill_items_table.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO items (id, text) VALUES 
+(1, "one"),
+(2, "two"),
+(3, "three");
